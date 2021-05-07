08e396d4497dd64f7a6e43f675b51f34c370d5da
kajetan <kajetanzdanowicz@wp.pl>
2021-05-07 21:45:58 +0200

    payment method

--- code/service/models.py
+++ code/service/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class PaymentMethod():
+    METHODS = (
+        ('CASH', _('CASH')),
+        ('BANK_TRANSFER', _('BANK TRANSFER')),
+        ('CREDIT_CARD', _('CREDIT CARD')),
+    )
+
+
+class Payment(models.Model):
+    payment_method = models.CharField(_('Payment method'), max_length=20,
+                                                choices=PaymentMethod.METHODS)
