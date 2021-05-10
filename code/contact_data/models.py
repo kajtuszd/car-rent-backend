f2e001a1c889e7da29fd0f16f4a09deeaa6d07cf
kajetan <kajetanzdanowicz@wp.pl>
2021-05-11 00:49:45 +0200

    address model

--- code/contact_data/models.py
+++ code/contact_data/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.core.validators import EmailValidator, RegexValidator
-from django_extensions.db.fields import AutoSlugField
+from django.core.validators import (EmailValidator, MaxValueValidator,
+                                    MinValueValidator, RegexValidator)
 from django.utils.translation import ugettext_lazy as _
 from utils.slugify import generate_slug
 
@@ -21,6 +21,32 @@ class Contact(models.Model):
     class Meta:
         verbose_name = _('contact')
         verbose_name_plural = _('contacts')
+
+    def __str__(self):
+        return f'{self.slug_field}'
+
+
+class Address(models.Model):
+    city = models.CharField(_('City'), max_length=30, null=True)
+    street = models.CharField(_('Street'), max_length=30, null=True)
+    house_number = models.IntegerField(_('House number'), null=True,
+                                       validators=[MaxValueValidator(1000),
+                                                   MinValueValidator(1)])
+    flat_number = models.IntegerField(_('Flat number'), null=True, blank=True,
+                                      validators=[MaxValueValidator(100),
+                                                  MinValueValidator(1)])
+    zip_code = models.CharField(_('Zip code'), null=True, max_length=6,
+                                validators=[RegexValidator(r'^\d{2}-\d{3}$',
+                                                           _('Zip code must '
+                                                               'be entered in '
+                                                               'the format '
+                                                               '12-345.'))])
+    slug_field = models.CharField(_('Slug'), default=generate_slug, unique=True,
+                                  max_length=7, db_index=True, editable=False)
+
+    class Meta:
+        verbose_name = _('address')
+        verbose_name_plural = _('addresses')
 
     def __str__(self):
         return f'{self.slug_field}'
