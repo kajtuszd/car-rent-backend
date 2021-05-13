cc4a39969a7d2dd211c455178e1960c822584bdc
kajetan <kajetanzdanowicz@wp.pl>
2021-05-13 19:23:00 +0200

    refactor

--- code/customer/admin.py
+++ code/customer/admin.py
@@ -6,10 +6,12 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     model = User
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('driver_license_id', 'personal_id', 'address', 'contact',)}),
+        (None, {'fields': (
+            'driver_license_id', 'personal_id', 'address', 'contact',)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('email', 'first_name', 'last_name',)}),
     )
+
 
 admin.site.register(User, CustomUserAdmin)
--- code/customer/models.py
+++ code/customer/models.py
@@ -10,13 +10,20 @@ from django.utils.translation import ugettext_lazy as _
 class User(AbstractUser):
     email = models.EmailField(_('Email address'), unique=True)
     driver_license_id = models.CharField(_('Driver license ID'), max_length=8,
-                validators=[RegexValidator(r'^[A-Z0-9]{8}$', _('Enter your unique ID') )],
-                      unique=True, blank=False, null=True)
+                                         validators=[
+                                             RegexValidator(r'^[A-Z0-9]{8}$',
+                                                            _('Enter your '
+                                                              'unique ID'))],
+                                         unique=True, blank=False, null=True)
     personal_id = models.CharField(_('Personal ID'), max_length=11,
-                   validators=[RegexValidator(r'^[0-9]{11}$', _('Enter your unique ID') )],
-                 unique=True, blank=False, null=True)
-    address = models.OneToOneField(Address, on_delete=models.PROTECT, blank=False, null=True)
-    contact = models.OneToOneField(Contact, on_delete=models.PROTECT, blank=False, null=True)
+                                   validators=[RegexValidator(r'^[0-9]{11}$',
+                                                              _('Enter your '
+                                                                'unique ID'))],
+                                   unique=True, blank=False, null=True)
+    address = models.OneToOneField(Address, on_delete=models.PROTECT,
+                                   blank=False, null=True)
+    contact = models.OneToOneField(Contact, on_delete=models.PROTECT,
+                                   blank=False, null=True)
 
     class Meta:
         verbose_name = _('user')
