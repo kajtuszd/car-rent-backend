753911e58bae806fd44821defad4cba670d9a723
kajetan <kajetanzdanowicz@wp.pl>
2021-05-30 23:56:40 +0200

    register template bug fix

--- code/customer/forms.py
+++ code/customer/forms.py
@@ -5,18 +5,20 @@ from django.contrib.auth import get_user_model
 
 
 class CustomUserCreationForm(UserCreationForm):
-    def register_user(request):
-        if request.method == 'POST':
-            form = CustomUserCreationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                username = form.cleaned_data.get('username')
-                messages.success(request, f'Created account for {username}')
-                return redirect('car_rent-home')
-        else:
-            form = UserCreationForm()
-        return render(request, 'user/user_register.html', {'form': form})
 
     class Meta:
         model = get_user_model()
         fields = ('username', 'email')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Created account for {username}')
+            return redirect('car_rent-home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'user/user_register.html', {'form': form})
--- code/customer/urls.py
+++ code/customer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import UserListView, UserDetailView
-from .forms import CustomUserCreationForm
+from .forms import register_user
 
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
     path('user/<str:slug>/', UserDetailView.as_view(), name='user-detail'),
-    path('register/', CustomUserCreationForm.register_user, name='register'),
+    path('register/', register_user, name='register'),
 ]
